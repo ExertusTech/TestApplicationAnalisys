95c5d30b7c5cac487ac3a4636a990499430a65c9
Romeo Martinez <romeo_martinez@crgl_thirdpart.com>
2022-09-08 13:30:05 -0500

    comit

---
+++ Common/DataAccess/ConnectionService.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DRP.Infra.DataAccess.Core
+{
+    public interface IConnectionService
+    {
+        public IEnumerable<dynamic> ProcedureQuery(string procedureName, object parameters);
+
+        public dynamic ProcedureQuerySingle(string procedureName, object parameters);
+    }
+
+    public class ConnectionService : IConnectionService
+    {
+        public ConnectionService(IConfiguration config)
+        {
+            this.Sql = new SqlConnection(config.GetConnectionString("Main"));
+        }
+
+        public SqlConnection Sql { get; set; }
+
+        public IEnumerable<dynamic> ProcedureQuery(string procedureName, object parameters)
+        {
+            return this.Sql.Query(procedureName, parameters, commandType: CommandType.StoredProcedure);
+        }
+
+        public dynamic ProcedureQuerySingle(string procedureName, object parameters)
+        {
+            return this.Sql.QuerySingle(procedureName, parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+}

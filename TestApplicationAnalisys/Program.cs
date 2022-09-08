5df4a2e8e22725e085750888731f87032b8801d5
Romeo Martinez <romeo_martinez@crgl_thirdpart.com>
2022-09-08 15:12:20 -0500

    prueba

--- TestApplicationAnalisys/Program.cs
+++ TestApplicationAnalisys/Program.cs
@@ -1,5 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.AddControllers();
+
 var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 
---
+++ TestApplicationAnalisys/TestController.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using DRP.Infra.DataAccess.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApplicationAnalisys
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public TestController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Get()
+        {
+            return Ok("prueba");
+        }
+
+        [Route("bd/{query}")]
+        public IActionResult Get(string query)
+        {
+            var servicio = new ConnectionService(_config);
+            var result = servicio.Sql.Query($"select * from test where otro = '{query}'");
+            return Ok(result);
+        }
+
+    }
+}

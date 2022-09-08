deace4a66c8fdecb892f43fbe54e39baca8532d1
Romeo Martinez <romeo_martinez@crgl_thirdpart.com>
2022-09-08 09:23:46 -0500

    Update DomainEvents.cs

--- Utility/Negocio/DomainEvents.cs
+++ Utility/Negocio/DomainEvents.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Utility.Negocio
@@ -8,23 +7,14 @@ namespace Utility.Negocio
     {
         private static List<Type>? _handlers;
         private static IServiceProvider? _serviceProvider;
-        private static IHttpContextAccessor? _httpContextAccessor;
 
-        private static IHttpContextAccessor? HttpContextAccessor
-        {
-            get
-            {
-                return _httpContextAccessor ??= _serviceProvider?.GetService(typeof(IHttpContextAccessor)) as IHttpContextAccessor;
-            }
-        }
-
-        public static void Init(IServiceProvider? serviceProvider, Assembly assembly)
+        public static void Init(IServiceProvider? serviceProvider, Assembly? assembly = null)
         {
             _serviceProvider = serviceProvider;
 
-            _handlers = assembly
-                .GetTypes()
-                .Where(x => x.IsInterface &&  x.GetInterfaces().Any(y => y.IsGenericType && y.GetGenericTypeDefinition() == typeof(IHandler<>)))
+            _handlers = (assembly ?? Assembly.GetExecutingAssembly())
+                .GetTypes() 
+                .Where(x => x.GetInterfaces().Any(y => y.IsGenericType && y.GetGenericTypeDefinition() == typeof(IHandler<>)))
                 .ToList();
         }
 
@@ -41,42 +31,14 @@ namespace Utility.Negocio
 
                 if (!canHandleEvent) continue;
 
-                dynamic handler = HttpContextAccessor?.HttpContext.RequestServices.GetService(handlerType);
+                dynamic handler = _serviceProvider?.GetRequiredService(handlerType)!;
 
                 if (handler == null)
                 {
-                    handler = Activator.CreateInstance(handlerType) ?? ;
+                    return;
                 }
 
                 handler.Handle((dynamic)domainEvent);
-            }
-        }
-
-        public static void Raise<T>(T args) where T : IDomainEvent
-        {
-            var domainEvent = args;
-            if (_handlers ==null)
-            {
-                return;
-            }
-
-            foreach (var handlerType in _handlers)
-            {
-                var canHandleEvent = handlerType.GetInterfaces()
-                    .Any(x => x.IsGenericType
-                              && x.GetGenericTypeDefinition() == typeof(IHandler<>)
-                              && x.GenericTypeArguments[0] == domainEvent.GetType());
-
-                if (!canHandleEvent) continue;
-
-                dynamic handler = HttpContextAccessor.HttpContext.RequestServices.GetService(handlerType);
-
-                if (handler == null)
-                {
-                    handler = Activator.CreateInstance(handlerType);
-                }
-
-                handler.Handle((dynamic) domainEvent);
             }
         }
     }
